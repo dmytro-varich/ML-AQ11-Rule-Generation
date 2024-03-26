9a63cb578c82d91477af3e80dd7e5295614abcfe
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-03-26 15:16:04 +0100

    Improved preprocess & separation

--- aq11_algorithm.py
+++ aq11_algorithm.py
@@ -4,40 +4,58 @@ from ucimlrepo import fetch_ucirepo
 from sklearn.model_selection import train_test_split
 
 def preprocess_data(dataset):
-    df = dataset.data.original
+    #
+    df = dataset.data.original.copy()
 
+    #
+    selected_categories = ['technician', 'entrepreneur', 'student', 'self-employed', 'retired']
+    df = df.loc[df['job'].isin(selected_categories)]
+    
+    #
     bins = [17, 44, 59, 74, 100]
     labels = [0, 1, 2, 3]
     df['age'] = pd.cut(df['age'], bins=bins, labels=labels)
     
-    bins = [-8019, 0, 5000, 10000, 15000, 20000, 102127]
+    #
+    bins = [float('-inf'), 0, 5000, 10000, 15000, 20000, float('inf')]
     labels = [0, 1, 2, 3, 4, 5]
     df['balance'] = pd.cut(df['balance'], bins=bins, labels=labels)
 
-    bins = [0, 60, 120, 300, 600, 900, 1800, 3600, 4918]
-    labels = [0, 1, 2, 3, 4, 5, 6, 7]
+    #
+    bins = [0, 60, 300, 900, 1800, float('inf')]
+    labels = [0, 1, 2, 3, 4]
     df['duration'] = pd.cut(df['duration'], bins=bins, labels=labels)
 
+    #
     categorical_attributes = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'poutcome', 'y']
     for column in categorical_attributes:
         df[column] = df[column].astype('category').cat.codes
 
+    #
     df = df.dropna()
 
     return df
 
 def separation_data(data) -> dict:
-    my_attributes = ['age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan', 'contact', 'duration']
-    E1 = data[data['y'] == 1][my_attributes][:80]
-    E2 = data[data['y'] == 0][my_attributes][:80]
+    #
+    my_attributes = ['age', 'job', 'marital', 'education', 'balance', 'housing', 'loan', 'contact', 'duration']
+    
+    #
+    E1 = data[data['y'] == 1][my_attributes]
+    E2 = data[data['y'] == 0][my_attributes]
 
-    E1_train, E1_test = train_test_split(E1, test_size=0.33, random_state=42)
-    E2_train, E2_test = train_test_split(E2, test_size=0.33, random_state=42)
+    # 
+    E1 = E1.sample(n=40)
+    E2 = E2.sample(n=40)
 
-    return {'E1': (E1_train, E1_test), 'E2': (E2_train, E2_test)}
+    # 
+    E1_train, E1_test = train_test_split(E1, test_size=0.5)
+    E2_train, E2_test = train_test_split(E2, test_size=0.5)
 
-def aq11():
-    pass
+    return {'E1': (E1_test, E1_train), 'E2': (E2_test, E2_train)}
+
+def aq11(E1, E2):
+    pass 
 
 def main():
     # 
@@ -45,9 +63,12 @@ def main():
 
     # 
     processed_dataset = preprocess_data(bank_marketing) 
-       
+   
     # 
     separated_data = separation_data(processed_dataset)
+
+    # 
+    aq11(separated_data['E1'][0], separated_data['E2'][0])
 
 if __name__ == '__main__':
     main()
