8c01b2ac4d650c9f6ccae374a3a64a18410f6583
Dmytro Varich <136006220+dmytro-varich@users.noreply.github.com>
2024-03-29 22:54:32 +0100

    First version of the finished algorithm

--- aq11_algorithm.py
+++ aq11_algorithm.py
@@ -56,29 +56,42 @@ def separation_data(data) -> dict:
 
 def aq11(E1, E2, Generated_Rules=None):
     if Generated_Rules is None:
-        E1_E2_rules = str()
+        rules_between_E1_and_E2 = list()
+
         for E1_index, E1_row in E1.iterrows():
-            E1_rules = list()
-            # condition 
-            # if ... then continue else ... mb down cykle
-            for E2_index, E2_row in E2.iterrows():
-                generate_rules_for_row: list[str] = list()
-                for E1_column, E1_value, _, E2_value in zip(E1_row.items(), E2_row.items()):
+            E1_generated_rules = list()
+
+            for _, E2_row in E2.iterrows():
+                generate_rules_for_row = str()
+
+                for (E1_column, E1_value), (_, E2_value) in zip(E1_row.items(), E2_row.items()):
+                    
+                    if E1_index > 0:
+                        metadata = dict()
+
+                        for column, value in E1_row.items():
+                            metadata[column] = value 
+
+                        for key, value in metadata.items():
+                            globals()[key] = value 
+                        
+                        if eval(' or '.join(rules_between_E1_and_E2)): 
+                            break 
+                     
                     if E1_value > E2_value:
-                        generate_rules_for_row.append(f"{E1_column} > {E2_value}")                
+                        generate_rules_for_row += f"{E1_column} > {E2_value} or "                
                     elif E1_value < E2_value: 
-                        generate_rules_for_row.append(f"{E1_column} < {E2_value}") 
+                        generate_rules_for_row += f"{E1_column} < {E2_value} or " 
                     else:
                         continue
-                E1_rules.append(generate_rules_for_row)
 
-            # for rules in E1_rules:
-            #     for i=1, rule in rules:
-                        
-            # E1_E2_rules.append()
-                           
+                E1_generated_rules.append("(" + generate_rules_for_row[:-4] + ")")
+            rules_between_E1_and_E2.append("(" + ' and '.join(E1_generated_rules) + ")")
+        
+        Generated_Rules = ' or '.join(rules_between_E1_and_E2).replace("() and ()", "").replace("  and  ", "").replace("andand", "").replace("( and )", " ").replace(" or  ", "")
+        return Generated_Rules
     else: 
-        pass # train data -> evaluation the models
+        pass # test data -> evaluation the models
 
 def main():
     # 
@@ -89,11 +102,11 @@ def main():
     separated_data = separation_data(processed_dataset)
 
     # 
-    E1_test, E2_test = separated_data['E1'][0], separated_data['E2'][0]
+    E1_test, E2_test = separated_data['E1'][0].reset_index(drop=True), separated_data['E2'][0].reset_index(drop=True)
     Generated_Rules = aq11(E1_test, E2_test)
-
+    print(Generated_Rules)
     # 
-    # E1_train, E2_train = separated_data['E1'][1], separated_data['E2'][1]
+    # E1_train, E2_train = separated_data['E1'][1].reset_index(drop=True), separated_data['E2'][1].reset_index(drop=True)
     # aq11(E1_train, E2_train, Generated_Rules)
 
 
